fd4cb58af30a46cc020854326808760cdc06a3fe
Shehab Taher <shehab.taher93@gmail.com>
2022-10-29 18:24:45 +0200

    classes&pointers Section

--- Classes & Pointers/Calsses.js
+++ Classes & Pointers/Calsses.js
@@ -2,5 +2,20 @@ class Cookie {
     constructor(color){
         this.color=color
     }
+    getColor(){
+        return this.color
+    }
+    setColor(color){
+        this.color = color
+    }
 }
+
+
 let cookieOne = new Cookie('green')
+let cookieTwo = new Cookie('blue')
+
+console.log(cookieOne.getColor())
+console.log(cookieTwo.getColor())
+
+cookieOne.setColor("yellow")
+console.log(cookieOne.getColor())
